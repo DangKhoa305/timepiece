ec7f04cc97857f492591765699ece17ba55fc24d
AnhKhoa <nguyenanhkhoa1699@gmail.com>
2024-06-05 20:51:10 +0700

    fix getter setter

--- src/main/java/app/timepiece/dto/ShowWatchDTO.java
+++ src/main/java/app/timepiece/dto/ShowWatchDTO.java
@@ -1,5 +1,14 @@
 package app.timepiece.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ShowWatchDTO {
     private Long id;
     private String name;
@@ -7,52 +16,4 @@ public class ShowWatchDTO {
     private String status;
     private String imageUrl;
 
-
-    public ShowWatchDTO(Long id, String name, double price, String status, String imageUrl) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.status = status;
-        this.imageUrl = imageUrl;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
 }
--- src/main/java/app/timepiece/dto/WatchDTO.java
+++ src/main/java/app/timepiece/dto/WatchDTO.java
@@ -1,6 +1,15 @@
 package app.timepiece.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class WatchDTO {
 
     private Long id;
@@ -19,124 +28,5 @@ public class WatchDTO {
     private String watchTypeName;
     private List<String> imageUrls;
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getBrandName() {
-        return brandName;
-    }
-
-    public void setBrandName(String brandName) {
-        this.brandName = brandName;
-    }
-
-    public int getYearProduced() {
-        return yearProduced;
-    }
-
-    public void setYearProduced(int yearProduced) {
-        this.yearProduced = yearProduced;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getMaterial() {
-        return material;
-    }
-
-    public void setMaterial(String material) {
-        this.material = material;
-    }
-
-    public String getWatchStrap() {
-        return watchStrap;
-    }
-
-    public void setWatchStrap(String watchStrap) {
-        this.watchStrap = watchStrap;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
-    }
-
-    public String getAccessories() {
-        return accessories;
-    }
-
-    public void setAccessories(String accessories) {
-        this.accessories = accessories;
-    }
-
-    public String getReferenceCode() {
-        return referenceCode;
-    }
-
-    public void setReferenceCode(String referenceCode) {
-        this.referenceCode = referenceCode;
-    }
-
-    public String getWatchTypeName() {
-        return watchTypeName;
-    }
-
-    public void setWatchTypeName(String watchTypeName) {
-        this.watchTypeName = watchTypeName;
-    }
-
-    public List<String> getImageUrls() {
-        return imageUrls;
-    }
-
-    public void setImageUrls(List<String> imageUrls) {
-        this.imageUrls = imageUrls;
-    }
 }

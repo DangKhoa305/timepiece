62b2cc6907f86de8530cda63a849b39316fcd6b3
AnhKhoa <nguyenanhkhoa1699@gmail.com>
2024-06-19 17:16:13 +0700

    search watch

---
+++ src/main/java/app/timepiece/dto/SearchWatchDTO.java
@@ -0,0 +1,19 @@
+package app.timepiece.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SearchWatchDTO {
+    private Long id;
+    private String name;
+    private String watchstatus;
+    private double price;
+    private String watchImage;
+    private String status;
+
+
+}
--- src/main/java/app/timepiece/repository/WatchRepository.java
+++ src/main/java/app/timepiece/repository/WatchRepository.java
@@ -3,6 +3,8 @@ import app.timepiece.entity.Watch;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,22 @@ import java.util.List;
 public interface WatchRepository extends JpaRepository<Watch, Long> {
     List<Watch> findByNameContainingIgnoreCase(String name);
     List<Watch> findByUserIdAndStatus(Long userId, String status);
+
+    @Query("SELECT w FROM Watch w " +
+            "WHERE (:price IS NULL OR w.price = :price) " +
+            "AND (:address IS NULL OR w.address = :address) " +
+            "AND (:type IS NULL OR w.watchType.typeName = :type) " +
+            "AND (:brand IS NULL OR w.brand.brandName = :brand) " +
+            "AND (:watchStatus IS NULL OR w.watchStatus = :watchStatus)" +
+            "AND (:status IS NULL OR w.status = :status)")
+    Page<Watch> searchWatches(
+            @Param("price") Double price,
+            @Param("address") String address,
+            @Param("type") String type,
+            @Param("brand") String brand,
+            @Param("watchStatus") String watchStatus,
+            @Param("status") String status,
+            Pageable pageable
+    );
+
 }

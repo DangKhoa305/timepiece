a970c41508a66765411345f6001ac1a6b421b5ea
DangKhoa305 <105625628+DangKhoa305@users.noreply.github.com>
2024-06-22 20:57:00 +0700

    fix api AppraisalRequest (#35)

--- src/main/java/app/timepiece/repository/AppraisalRequestRepository.java
+++ src/main/java/app/timepiece/repository/AppraisalRequestRepository.java
@@ -5,6 +5,7 @@ import app.timepiece.entity.AppraisalRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +14,6 @@ import java.util.List;
 public interface AppraisalRequestRepository extends JpaRepository<AppraisalRequest, Long> {
     Page<AppraisalRequest> findAllByStatus(String status, Pageable pageable);
     Page<AppraisalRequest> findAll(Pageable pageable);
-
+    @Query("SELECT COALESCE(MAX(ar.code), 0) FROM AppraisalRequest ar")
+    Long findMaxCode();
 }

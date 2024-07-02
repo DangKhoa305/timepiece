0a49b5ba1cd7523d7429c7028465b9267e7d9968
AnhKhoa <nguyenanhkhoa1699@gmail.com>
2024-07-02 22:55:33 +0700

    validate update user profile

--- src/main/java/app/timepiece/controller/UserController.java
+++ src/main/java/app/timepiece/controller/UserController.java
@@ -3,9 +3,14 @@ package app.timepiece.controller;
 import app.timepiece.dto.UpdateUserDTO;
 import app.timepiece.dto.UserDTO;
 import app.timepiece.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.stream.Collectors;
 
 @CrossOrigin
 @RestController
@@ -21,9 +26,22 @@ public class UserController {
         return ResponseEntity.ok(userDTO);
     }
 
-    @PutMapping("/{id}/UpdateUserProfile")
-    public ResponseEntity<UserDTO> updateUserById(@PathVariable Long id, @RequestBody UpdateUserDTO updateUserDTO) {
-        UserDTO userDTO = userService.updateUserById(id, updateUserDTO);
-        return ResponseEntity.ok(userDTO);
+    @PutMapping(value = "/{id}/UpdateUserProfile", consumes = {"multipart/form-data"})
+    public ResponseEntity<?> updateUserById(@PathVariable Long id, @Valid @ModelAttribute UpdateUserDTO updateUserDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            String errors = bindingResult.getFieldErrors().stream()
+                    .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                    .collect(Collectors.joining("\n"));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Validation errors:\n" + errors);
+        }
+
+        try {
+            UserDTO userDTO = userService.updateUserById(id, updateUserDTO);
+            return ResponseEntity.ok(userDTO);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error");
+        }
     }
 }

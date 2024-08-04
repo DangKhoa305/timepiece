dc674ba5a07d0b35c60648b5b9c846ba686a9818
Dangkhoa305 <dangkhoa1699@gmail.com>
2024-08-04 11:17:54 +0700

    validate api createOrder

--- src/main/java/app/timepiece/repository/OrderRepository.java
+++ src/main/java/app/timepiece/repository/OrderRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
@@ -14,4 +15,6 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("SELECT o FROM Order o WHERE o.watch.user.id = :sellerId")
     List<Order> findOrdersBySellerId(@Param("sellerId") Long sellerId);
+
+    Optional<Order> findByUserIdAndWatchId(Long userId, Long watchId);
 }
